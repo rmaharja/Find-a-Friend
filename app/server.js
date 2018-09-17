b9c6b510265d09594526839245a949e383192c48
rmaharja@gmail.com <rmaharja@gmail.com>
2018-09-17 07:43:06 -0400

    Added all files, working on Survey

---
+++ app/server.js
@@ -0,0 +1,41 @@
+// boilerplate
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+/*
+app.use= applying to everything (put, get...)
+.use= passing middleware
+*/
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, 'survey.html'));
+});
+
+// api Tables List
+app.get('/api/friends-list', function (req, res) {
+    return res.json(friends-list);
+});
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+/* 
+
+ */
